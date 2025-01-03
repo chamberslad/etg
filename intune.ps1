6b7c4fd574122516bd910f5e534159cd7d186885
chamberslad <lee.d.chambers@gmail.com>
2025-01-03 16:20:30 +0000

    Update intune.ps1

--- intune.ps1
+++ intune.ps1
@@ -1,7 +1,5 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 Install-Module Microsoft.Graph -Force
-Install-Module Microsoft.Graph.Authentication -Force
-Install-Module Microsoft.Graph.Groups -Force
 Connect-MgGraph -Scopes "DeviceManagementServiceConfig.ReadWrite.All"
 ##Log in with Intune account
 Install-script Get-WindowsAutopilotInfo -verbose
