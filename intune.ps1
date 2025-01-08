44daa414a583124cbf4af3229fa5a04ee886ba5d
chamberslad <lee.d.chambers@gmail.com>
2025-01-08 17:49:42 +0000

    Update intune.ps1

--- intune.ps1
+++ intune.ps1
@@ -4,4 +4,8 @@ Connect-MgGraph -Scopes "DeviceManagementServiceConfig.ReadWrite.All"
 ##Log in with Intune account
 Install-script Get-WindowsAutopilotInfo -verbose
 get-WindowsAutopilotInfo -online
+Write-Host "Navigate to the M3656 tenant and if needed assign the correct Enrolment profile to the device via Entra group.  When ready push Y to continue.  This WILL REBOOT the deivce."
+do {
+    $key = Read-Host
+} while ($key -ne 'y')
 restart-computer -force
